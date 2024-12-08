f3e787e6d0d4371ea8195ec35c4a88c53199b510
Tristan Wettich <tw.develop@mailbox.org>
2024-12-08 12:51:22 +0100

    #23 Solution 11 Grid refactoring

--- 23/11/src/main.zig
+++ 23/11/src/main.zig
@@ -38,7 +38,7 @@ pub fn main() !void {
 }
 
 pub fn solveChallenge(allocator: Allocator, input: []const u8, factor_a: u64, factor_b: u64) [2]u64 {
-    var map = grid.CharGrid.init(allocator, input);
+    var map = grid.ByteGrid.parse(allocator, input);
     defer map.deinit();
 
     var empty_cols = std.AutoArrayHashMap(u64, void).init(allocator);
@@ -48,22 +48,22 @@ pub fn solveChallenge(allocator: Allocator, input: []const u8, factor_a: u64, fa
     }
     var empty_rows: u64 = 0;
 
-    var galaxies_one = std.ArrayList(grid.Point).init(allocator);
+    var galaxies_one = std.ArrayList(grid.Vec2).init(allocator);
     defer galaxies_one.deinit();
 
-    var galaxies_two = std.ArrayList(grid.Point).init(allocator);
+    var galaxies_two = std.ArrayList(grid.Vec2).init(allocator);
     defer galaxies_two.deinit();
 
     // Read galaxies and expand space
     for (0..map.height) |y| {
         var empty: bool = true;
         for (0..map.width) |x| {
-            var p = grid.Point{ .x = @intCast(x), .y = @intCast(y) };
+            var p = grid.Vec2{ .x = @intCast(x), .y = @intCast(y) };
             if (map.get(p).? == '#') {
                 _ = empty_cols.fetchOrderedRemove(@intCast(x));
                 empty = false;
-                galaxies_one.append(p.translate(grid.Point{ .x = 0, .y = @intCast(empty_rows * (factor_a - 1)) })) catch unreachable;
-                galaxies_two.append(p.translate(grid.Point{ .x = 0, .y = @intCast(empty_rows * (factor_b - 1)) })) catch unreachable;
+                galaxies_one.append(p.translate(grid.Vec2{ .x = 0, .y = @intCast(empty_rows * (factor_a - 1)) })) catch unreachable;
+                galaxies_two.append(p.translate(grid.Vec2{ .x = 0, .y = @intCast(empty_rows * (factor_b - 1)) })) catch unreachable;
             }
         }
         if (empty) {
@@ -74,8 +74,8 @@ pub fn solveChallenge(allocator: Allocator, input: []const u8, factor_a: u64, fa
     while (empty_cols.popOrNull()) |col| {
         for (0..galaxies_one.items.len) |i| {
             if (galaxies_one.items[i].x > col.key and galaxies_one.items[i].x < last) {
-                galaxies_one.items[i] = galaxies_one.items[i].translate(grid.Point{ .x = @intCast((empty_cols.keys().len + 1) * (factor_a - 1)), .y = 0 });
-                galaxies_two.items[i] = galaxies_two.items[i].translate(grid.Point{ .x = @intCast((empty_cols.keys().len + 1) * (factor_b - 1)), .y = 0 });
+                galaxies_one.items[i] = galaxies_one.items[i].translate(grid.Vec2{ .x = @intCast((empty_cols.keys().len + 1) * (factor_a - 1)), .y = 0 });
+                galaxies_two.items[i] = galaxies_two.items[i].translate(grid.Vec2{ .x = @intCast((empty_cols.keys().len + 1) * (factor_b - 1)), .y = 0 });
             }
         }
         last = @intCast(col.key);
