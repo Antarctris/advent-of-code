d6414ba059379898a3c779d55f8d9cfb7c9369f5
Tristan Wettich <tw.develop@mailbox.org>
2024-12-13 23:50:26 +0100

    #24 Add JSON record of time measurements

--- 24/src/main.zig
+++ 24/src/main.zig
@@ -7,22 +7,13 @@ const print = std.debug.print;
 
 const solutions = @import("solutions.zig");
 
-fn readInputFile(allocator: Allocator, path: []const u8) ![]u8 {
-    var file = try std.fs.cwd().openFile(path, .{});
-    defer file.close();
-    const file_size = (try file.metadata()).size();
-    const input = try allocator.alloc(u8, file_size);
-    _ = try file.readAll(input);
-    return input;
-}
-
 pub fn main() !void {
     if (std.os.argv.len != 2) {
         std.debug.print("Number of day must be specified as first argument! No input read.\n", .{});
         std.process.exit(1);
     }
 
-    // Set up output
+    // Set up output (yes, not buffered because I can't be bothered)
     const stdout = std.io.getStdOut().writer();
 
     // Initialze allocator
@@ -38,7 +29,7 @@ pub fn main() !void {
         // Read input
         var path_buf: [12]u8 = undefined;
         const path = try std.fmt.bufPrint(&path_buf, "input/{d:0>2}.txt", .{day});
-        const input = try readInputFile(allocator, path);
+        const input = try std.fs.cwd().readFileAlloc(allocator, path, 1 << 16); // 64MiB
 
         try stdout.print("Processing...\n", .{});
 
@@ -46,7 +37,7 @@ pub fn main() !void {
             const time0_one = try time.Instant.now();
             const value_one = solution.part_one(allocator, input);
             const time1_one = try time.Instant.now();
-            const timed_one: f64 = @floatFromInt(time1_one.since(time0_one));
+            const timed_one: f32 = @floatFromInt(time1_one.since(time0_one));
             const elapsed_ms_one = timed_one / time.ns_per_ms;
             if (value_one) |value| {
                 try stdout.print("Part 1: {d}, {d:.3} ms\n", .{ value, elapsed_ms_one });
@@ -54,11 +45,18 @@ pub fn main() !void {
             const time0_two = try time.Instant.now();
             const value_two = solution.part_two(allocator, input);
             const time1_two = try time.Instant.now();
-            const timed_two: f64 = @floatFromInt(time1_two.since(time0_two));
+            const timed_two: f32 = @floatFromInt(time1_two.since(time0_two));
             const elapsed_ms_two = timed_two / time.ns_per_ms;
             if (value_two) |value| {
                 try stdout.print("Part 2: {d}, {d:.3} ms\n", .{ value, elapsed_ms_two });
             }
+
+            try updateRecord(allocator, ResultRecord{
+                .id = @intCast(day),
+                .title = solution.title(),
+                .part_one = if (value_one != null) elapsed_ms_one else null,
+                .part_two = if (value_two != null) elapsed_ms_two else null,
+            });
         }
 
         try stdout.print("Done!\n", .{});
@@ -66,4 +64,79 @@ pub fn main() !void {
         try stdout.print("Argument given was not a number of day! Input not read.", .{});
         std.process.exit(1);
     }
+}
+
+const record_path = "solution_record.json";
+fn updateRecord(allocator: Allocator, record: ResultRecord) !void {
+    if (std.fs.cwd().readFileAlloc(allocator, record_path, 1 << 12)) |old_file| { // 4MiB
+        defer allocator.free(old_file);
+
+        const old_record = try std.json.parseFromSlice(YearRecord, allocator, old_file, .{});
+        defer old_record.deinit();
+
+        var result_records = std.ArrayList(ResultRecord).init(allocator);
+        defer result_records.deinit();
+        try result_records.appendSlice(old_record.value.results);
+        std.sort.heap(ResultRecord, result_records.items, {}, resultRecordFirst);
+
+        var index: usize = 0;
+        while (index < result_records.items.len and result_records.items[index].id < record.id) {
+            index += 1;
+        }
+        if (result_records.items[index].id == record.id) {
+            result_records.items[index].update(record);
+        } else {
+            try result_records.insert(index, record);
+        }
+
+        const new_record = YearRecord{ .lang = ziglang, .results = result_records.items };
+        const new_json = try std.json.stringifyAlloc(allocator, new_record, .{ .whitespace = .indent_4 });
+
+        const file = try std.fs.cwd().createFile(record_path, .{});
+        defer file.close();
+
+        _ = try file.writeAll(new_json);
+    } else |_| {
+        var records: [1]ResultRecord = .{record};
+        const new_record = YearRecord{ .lang = ziglang, .results = &records };
+        const new_json = try std.json.stringifyAlloc(allocator, new_record, .{ .whitespace = .indent_4 });
+
+        const file = try std.fs.cwd().createFile(record_path, .{});
+        defer file.close();
+
+        _ = try file.writeAll(new_json);
+    }
+}
+
+const ziglang: []const u8 = "Zig";
+
+const YearRecord = struct {
+    lang: []const u8,
+    results: []ResultRecord,
+};
+
+const ResultRecord = struct {
+    id: u32,
+    title: []const u8,
+    part_one: ?f32,
+    part_two: ?f32,
+
+    pub fn update(self: *ResultRecord, other: ResultRecord) void {
+        self.title = other.title;
+        if (other.part_one) |new_one| {
+            if (self.part_one == null or new_one < self.part_one.?) {
+                self.part_one = new_one;
+            }
+        }
+        if (other.part_two) |new_two| {
+            if (self.part_two == null or new_two < self.part_two.?) {
+                self.part_two = new_two;
+            }
+        }
+    }
+};
+
+fn resultRecordFirst(ctx: void, a: ResultRecord, b: ResultRecord) bool {
+    _ = ctx;
+    return a.id < b.id;
 }
--- 24/src/solutions/solution.zig
+++ 24/src/solutions/solution.zig
@@ -5,11 +5,15 @@ const Solution = @This();
 vtable: *const VTable,
 
 pub const VTable = struct {
+    title: *const fn () []const u8,
     part_one: *const fn (allocator: std.mem.Allocator, input: []const u8) ?u64,
     part_two: *const fn (allocator: std.mem.Allocator, input: []const u8) ?u64,
 
     pub fn init(T: type) *const VTable {
         const table = &struct {
+            fn title() []const u8 {
+                return T.title();
+            }
             fn part_one(allocator: std.mem.Allocator, input: []const u8) ?u64 {
                 return T.part_one(allocator, input);
             }
@@ -18,11 +22,16 @@ pub const VTable = struct {
             }
         };
         return &.{
+            .title = table.title,
             .part_one = table.part_one,
             .part_two = table.part_two,
         };
     }
 };
+
+pub fn title(self: Solution) []const u8 {
+    return self.vtable.title();
+}
 
 pub fn part_one(self: Solution, allocator: std.mem.Allocator, input: []const u8) ?u64 {
     return self.vtable.part_one(allocator, input);
