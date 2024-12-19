7104445505fb4b0a999a716d9905f1817d89f7ed
Tristan Wettich <tw.develop@mailbox.org>
2024-12-20 00:18:33 +0100

    #23 Solution 02

--- 23/README.md
+++ 23/README.md
@@ -1,3 +1,4 @@
 | 2024                                                          | Stars | Time (ms)  | Time (ms)  |
 |---------------------------------------------------------------|:-----:|-----------:|-----------:|
 | [Day 1: Trebuchet?!](src/solutions/day01.zig)                 |  ⭐⭐ |      0.107 |      0.302 |
+| [Day 2: Cube Conundrum](src/solutions/day02.zig)              |  ⭐⭐ |      0.155 |      0.154 |
--- 23/src/solutions/day02.zig
+++ 23/src/solutions/day02.zig
@@ -1,47 +1,24 @@
+const Self = @This();
+
+// imports
 const std = @import("std");
 const mem = std.mem;
 const Allocator = std.mem.Allocator;
-const assert = std.debug.assert;
-const print = std.debug.print;
 
 const util = @import("util");
+const Solution = @import("./solution.zig");
 
-fn readInputFile(allocator: Allocator, path: []const u8) ![]u8 {
-    var file = try std.fs.cwd().openFile(path, .{});
-    defer file.close();
-    const file_size = (try file.metadata()).size();
-    const input = try allocator.alloc(u8, file_size);
-    _ = try file.readAll(input);
-    return input;
+// interface
+pub const solution: Solution = .{ .vtable = Solution.VTable.init(Self) };
+
+pub fn title() []const u8 {
+    return "Day 2: Cube Conundrum";
 }
 
-pub fn main() !void {
-    assert(std.os.argv.len == 2);
-
-    // Set up output
-    const stdout = std.io.getStdOut().writer();
-
-    // Initialze allocator
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    // Read input
-    const path: []const u8 = mem.span(std.os.argv[1]);
-    const input = try readInputFile(allocator, path);
-
-    // Run challenge subroutine
-    const solution: [2]u64 = solveChallenge(allocator, input);
-
-    // Print solution
-    try stdout.print("Solution:\nPart 1: {d}\nPart 2: {d}\n", .{ solution[0], solution[1] });
-}
-
-pub fn solveChallenge(allocator: Allocator, input: []const u8) [2]u64 {
+pub fn part_one(allocator: Allocator, input: []const u8) ?u64 {
     _ = allocator;
     const limit = CubeSet{ .red = 12, .green = 13, .blue = 14 };
-    var sum_one: u32 = 0;
-    var sum_two: u32 = 0;
+    var total_possible_game_ids: u32 = 0;
     var line_iterator = mem.splitScalar(u8, input, '\n');
     while (line_iterator.next()) |line| {
         if (line.len == 0) continue;
@@ -55,11 +32,31 @@ pub fn solveChallenge(allocator: Allocator, input: []const u8) [2]u64 {
             minimumGameCubeSet = minimumGameCubeSet.maximum(draw);
         }
         if (draw_contained) {
-            sum_one += std.fmt.parseInt(u8, line[5..colon_index], 10) catch unreachable;
+            total_possible_game_ids += std.fmt.parseInt(u8, line[5..colon_index], 10) catch unreachable;
         }
-        sum_two += minimumGameCubeSet.power();
     }
-    return .{ sum_one, sum_two };
+    return total_possible_game_ids;
+}
+
+pub fn part_two(allocator: Allocator, input: []const u8) ?u64 {
+    _ = allocator;
+    const limit = CubeSet{ .red = 12, .green = 13, .blue = 14 };
+    var total_minimum_power: u32 = 0;
+    var line_iterator = mem.splitScalar(u8, input, '\n');
+    while (line_iterator.next()) |line| {
+        if (line.len == 0) continue;
+        const colon_index = mem.indexOfScalar(u8, line, ':') orelse unreachable;
+        var draw_iterator = mem.splitSequence(u8, line[colon_index + 2 ..], "; "); // 2: colon and whitespace
+        var draw_contained = true;
+        var minimumGameCubeSet = CubeSet{ .red = 0, .green = 0, .blue = 0 };
+        while (draw_iterator.next()) |draw_str| {
+            const draw = CubeSet.fromDraw(draw_str);
+            draw_contained = draw_contained and limit.contains(draw);
+            minimumGameCubeSet = minimumGameCubeSet.maximum(draw);
+        }
+        total_minimum_power += minimumGameCubeSet.power();
+    }
+    return total_minimum_power;
 }
 
 const CubeSet = struct {
@@ -105,6 +102,16 @@ const CubeSet = struct {
     }
 };
 
+test "part_1.sample_1" {
+    const result = part_one(std.testing.allocator, sample_1) orelse return error.SkipZigTest;
+    try std.testing.expectEqual(8, result);
+}
+
+test "part_2.sample_1" {
+    const result = part_two(std.testing.allocator, sample_1) orelse return error.SkipZigTest;
+    try std.testing.expectEqual(2286, result);
+}
+
 const sample_1: []const u8 =
     \\Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     \\Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -113,13 +120,3 @@ const sample_1: []const u8 =
     \\Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     \\
 ;
-
-test "part_1.sample_1" {
-    const solution = solveChallenge(std.testing.allocator, sample_1);
-    try std.testing.expectEqual(8, solution[0]);
-}
-
-test "part_2.sample_1" {
-    const solution = solveChallenge(std.testing.allocator, sample_1);
-    try std.testing.expectEqual(2286, solution[1]);
-}
