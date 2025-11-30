1261eb58c26b8e8c564828058b65e25b0668c075
Antarctris <tw.develop@mailbox.org>
2025-12-01 00:31:13 +0100

    #25 Switch to pre-built zig setup

--- 25/flake.nix
+++ 25/flake.nix
@@ -12,7 +12,7 @@
         flake-utils.follows = "flake-utils";
       };
     };
-    zls-overlay.url = "github:zigtools/zls/ce6c8f02c78e622421cfc2405c67c5222819ec03";
+    zls-overlay.url = "github:omega-800/zls-overlay";
   };
 
   outputs = {
@@ -27,9 +27,7 @@
         system: let
           pkgs = nixpkgs.legacyPackages.${system};
           zig = zig-overlay.packages.${system}."0.15.2";
-          zls = zls-overlay.packages.x86_64-linux.zls.overrideAttrs (old: {
-            nativeBuildInputs = [ zig ];
-          });
+          zls = zls-overlay.packages.${system}."0.15.0";
         in {
           devShells.${system}.default = pkgs.mkShell {
             packages = with pkgs; [
